9bb6526d790581c99d48efa85e645c1d5ffb4c8d
zalam003 <48445346+zalam003@users.noreply.github.com>
2020-09-12 16:13:11 -0400

    Add files via upload

--- scripts/build-avs.sh
+++ scripts/build-avs.sh
@@ -3,8 +3,8 @@
 sudo apt-get update
 
 cd /home/pi
-mkdir sdk_folder
-cd sdk_folder
+mkdir alexa-rpi
+cd alexa-rpi
 mkdir sdk-build sdk-source third-party sdk-install db
 
 sudo apt-get -y install \
@@ -12,7 +12,7 @@ sudo apt-get -y install \
    libssl-dev libsoup2.4-dev libgcrypt20-dev libgstreamer-plugins-bad1.0-dev \
    gstreamer1.0-plugins-good libasound2-dev doxygen
    
-cd $HOME/sdk_folder/third-party
+cd $HOME/alexa-rpi/third-party
 
 wget -c http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz
 tar zxf pa_stable_v190600_20161030.tgz
@@ -21,45 +21,45 @@ rm pa_stable_v190600_20161030.tgz
 cd portaudio
 ./configure --without-jack
 
-cd $HOME/sdk_folder/third-party/portaudio
+cd $HOME/alexa-rpi/third-party/portaudio
 make -j4
 
 pip install commentjson
 
-cd $HOME/sdk_folder/sdk-source
+cd $HOME/alexa-rpi/sdk-source
     
 git clone --single-branch --branch v1.20.0 git://github.com/alexa/avs-device-sdk.git
 
-cd $HOME/sdk_folder/third-party
+cd $HOME/alexa-rpi/third-party
 git clone git://github.com/Sensory/alexa-rpi.git
 
-cd $HOME/sdk_folder/third-party/alexa-rpi/bin/
+cd $HOME/alexa-rpi/third-party/alexa-rpi/bin/
 ./license.sh
 
-cd $HOME/sdk_folder/sdk-build
-cmake $HOME/sdk_folder/sdk-source/avs-device-sdk \
+cd $HOME/alexa-rpi/sdk-build
+cmake $HOME/alexa-rpi/sdk-source/avs-device-sdk \
  -DSENSORY_KEY_WORD_DETECTOR=ON \
- -DSENSORY_KEY_WORD_DETECTOR_LIB_PATH=$HOME/sdk_folder/third-party/alexa-rpi/lib/libsnsr.a \
- -DSENSORY_KEY_WORD_DETECTOR_INCLUDE_DIR=$HOME/sdk_folder/third-party/alexa-rpi/include \
+ -DSENSORY_KEY_WORD_DETECTOR_LIB_PATH=$HOME/alexa-rpi/third-party/alexa-rpi/lib/libsnsr.a \
+ -DSENSORY_KEY_WORD_DETECTOR_INCLUDE_DIR=$HOME/alexa-rpi/third-party/alexa-rpi/include \
  -DGSTREAMER_MEDIA_PLAYER=ON \
  -DPORTAUDIO=ON \
- -DPORTAUDIO_LIB_PATH=$HOME/sdk_folder/third-party/portaudio/lib/.libs/libportaudio.a \
- -DPORTAUDIO_INCLUDE_DIR=$HOME/sdk_folder/third-party/portaudio/include
+ -DPORTAUDIO_LIB_PATH=$HOME/alexa-rpi/third-party/portaudio/lib/.libs/libportaudio.a \
+ -DPORTAUDIO_INCLUDE_DIR=$HOME/alexa-rpi/third-party/portaudio/include
 
 #make SampleApp
 make
 
-BUILD_PATH=$HOME/sdk_folder/sdk-build
+BUILD_PATH=$HOME/alexa-rpi/sdk-build
 OUTPUT_CONFIG_FILE="$BUILD_PATH/Integration/AlexaClientSDKConfig.json"
 TEMP_CONFIG_FILE="$BUILD_PATH/Integration/tmp_AlexaClientSDKConfig.json"
 
-cp $HOME/Downloads/config.json $HOME/sdk_folder/sdk-source/avs-device-sdk/tools/Install
+cp $HOME/Downloads/config.json $HOME/alexa-rpi/sdk-source/avs-device-sdk/tools/Install
 
-cd $HOME/sdk_folder/sdk-source/avs-device-sdk/tools/Install 
+cd $HOME/alexa-rpi/sdk-source/avs-device-sdk/tools/Install 
 bash genConfig.sh config.json \
  123456 \
- $HOME/sdk_folder/db \
- $HOME/sdk_folder/sdk-source/avs-device-sdk \
+ $HOME/alexa-rpi/db \
+ $HOME/alexa-rpi/sdk-source/avs-device-sdk \
  $TEMP_CONFIG_FILE \
  -DSDK_CONFIG_MANUFACTURER_NAME="raspberrypi" \
  -DSDK_CONFIG_DEVICE_DESCRIPTION="raspberrypi"
@@ -84,7 +84,7 @@ sed -i -e 's/Vancouver/New_York/g' "$OUTPUT_CONFIG_FILE"
 # Delete temp file
 rm $TEMP_CONFIG_FILE
  
-cp $OUTPUT_CONFIG_FILE $HOME/sdk_folder/BACKUP-AlexaClientSDKConfig.json
+cp $OUTPUT_CONFIG_FILE $HOME/alexa-rpi/BACKUP-AlexaClientSDKConfig.json
 
 
 # Configure the microphone and speaker
@@ -105,6 +105,6 @@ EOF
 read -p "Verify the AVS Device SDK build [Y/n]: "
 
 # Step 6: Verify the AVS Device SDK build
-cd $HOME/sdk_folder/sdk-build
+cd $HOME/alexa-rpi/sdk-build
 #PA_ALSA_PLUGHW=1 ./SampleApp/src/SampleApp ./Integration/AlexaClientSDKConfig.json INFO
 PA_ALSA_PLUGHW=1 ./SampleApp/src/SampleApp ./Integration/AlexaClientSDKConfig.json ../third-party/alexa-rpi/models
--- scripts/build-screen.sh
+++ scripts/build-screen.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
 
 # Step 7: Download and build the APL Core Library
-cd $HOME/sdk_folder
+cd $HOME/alexa-rpi
 git clone --single-branch --branch v1.3.3 git://github.com/alexa/apl-core-library.git 
 
-cd $HOME/sdk_folder/apl-core-library
+cd $HOME/alexa-rpi/apl-core-library
 mkdir build
 cd build
 cmake ..
 make
 
 # Step 8: Install Alexa Smart Screen SDK dependencies
-cd $HOME/sdk_folder/third-party
+cd $HOME/alexa-rpi/third-party
 wget https://github.com/zaphoyd/websocketpp/archive/0.8.1.tar.gz -O websocketpp-0.8.1.tar.gz
 tar -xvzf websocketpp-0.8.1.tar.gz
 rm websocketpp-0.8.1.tar.gz
 
-cd $HOME/sdk_folder/third-party
+cd $HOME/alexa-rpi/third-party
 sudo apt-get -y install libasio-dev --no-install-recommends 
 
-cd $HOME/sdk_folder/third-party
+cd $HOME/alexa-rpi/third-party
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 
 # Step 9: Download and build the Alexa Smart Screen SDK
-cd $HOME/sdk_folder
+cd $HOME/alexa-rpi
 git clone git://github.com/alexa/alexa-smart-screen-sdk.git
 
-cp $HOME/Downloads/package-lock.json $HOME/sdk_folder/alexa-smart-screen-sdk/modules/GUI/js/.
+cp $HOME/Downloads/package-lock.json $HOME/alexa-rpi/alexa-smart-screen-sdk/modules/GUI/js/.
 
-cd $HOME/sdk_folder
+cd $HOME/alexa-rpi
 mkdir ss-build
 cd ss-build
 
-cmake -DCMAKE_PREFIX_PATH=$HOME/sdk_folder/sdk-install \
+cmake -DCMAKE_PREFIX_PATH=$HOME/alexa-rpi/sdk-install \
  -DSENSORY_KEY_WORD_DETECTOR=ON \
- -DSENSORY_KEY_WORD_DETECTOR_LIB_PATH=$HOME/sdk_folder/third-party/alexa-rpi/lib/libsnsr.a \
- -DSENSORY_KEY_WORD_DETECTOR_INCLUDE_DIR=$HOME/sdk_folder/third-party/alexa-rpi/include \
- -DWEBSOCKETPP_INCLUDE_DIR=$HOME/sdk_folder/third-party/websocketpp-0.8.1 \
+ -DSENSORY_KEY_WORD_DETECTOR_LIB_PATH=$HOME/alexa-rpi/third-party/alexa-rpi/lib/libsnsr.a \
+ -DSENSORY_KEY_WORD_DETECTOR_INCLUDE_DIR=$HOME/alexa-rpi/third-party/alexa-rpi/include \
+ -DWEBSOCKETPP_INCLUDE_DIR=$HOME/alexa-rpi/third-party/websocketpp-0.8.1 \
  -DDISABLE_WEBSOCKET_SSL=ON \
  -DGSTREAMER_MEDIA_PLAYER=ON \
  -DCMAKE_BUILD_TYPE=DEBUG \
- -DPORTAUDIO=ON -DPORTAUDIO_LIB_PATH=$HOME/sdk_folder/third-party/portaudio/lib/.libs/libportaudio.a \
- -DPORTAUDIO_INCLUDE_DIR=$HOME/sdk_folder/third-party/portaudio/include/ \
+ -DPORTAUDIO=ON -DPORTAUDIO_LIB_PATH=$HOME/alexa-rpi/third-party/portaudio/lib/.libs/libportaudio.a \
+ -DPORTAUDIO_INCLUDE_DIR=$HOME/alexa-rpi/third-party/portaudio/include/ \
  -DAPL_CORE=ON \
- -DAPLCORE_INCLUDE_DIR=$HOME/sdk_folder/apl-core-library/aplcore/include \
- -DAPLCORE_LIB_DIR=$HOME/sdk_folder/apl-core-library/build/aplcore \
- -DYOGA_INCLUDE_DIR=$HOME/sdk_folder/apl-core-library/build/yoga-prefix/src/yoga \
- -DYOGA_LIB_DIR=$HOME/sdk_folder/apl-core-library/build/lib \
+ -DAPLCORE_INCLUDE_DIR=$HOME/alexa-rpi/apl-core-library/aplcore/include \
+ -DAPLCORE_LIB_DIR=$HOME/alexa-rpi/apl-core-library/build/aplcore \
+ -DYOGA_INCLUDE_DIR=$HOME/alexa-rpi/apl-core-library/build/yoga-prefix/src/yoga \
+ -DYOGA_LIB_DIR=$HOME/alexa-rpi/apl-core-library/build/lib \
   ../alexa-smart-screen-sdk
   
 make
@@ -56,7 +56,7 @@ make
 read -p "Test the Alexa Smart Screen SDK sample app [Y/n]: "
 
 # Step 10: Test the Alexa Smart Screen SDK sample app
-cd $HOME/sdk_folder/ss-build
+cd $HOME/alexa-rpi/ss-build
 PA_ALSA_PLUGHW=1 ./modules/Alexa/SampleApp/src/SampleApp -C \
-$HOME/sdk_folder/sdk-build/Integration/AlexaClientSDKConfig.json -C \
-$HOME/sdk_folder/alexa-smart-screen-sdk/modules/GUI/config/SmartScreenSDKConfig.json -L INFO
+$HOME/alexa-rpi/sdk-build/Integration/AlexaClientSDKConfig.json -C \
+$HOME/alexa-rpi/alexa-smart-screen-sdk/modules/GUI/config/SmartScreenSDKConfig.json -L INFO
